f48510bc8bf11935245f0ca2920618783a43558c
HensalDeonCD <hensal@trusttech.in>
2024-05-21 10:49:53 +0530

    updated positions auto zoom

--- client/src/components/ModelViewer.jsx
+++ client/src/components/ModelViewer.jsx
@@ -3,43 +3,43 @@ import { OrbitControls, useGLTF } from "@react-three/drei";
 import { Suspense, useRef } from "react";
 
 const Model = ({ url }) => {
-    const { scene } = useGLTF(url);
-    const modelRef = useRef();
-    useFrame(() => {
-        if (modelRef.current) {
-            modelRef.current.rotation.y += 0.001;
-            modelRef.current.rotation.x += 0.001;
-        }
-    });
-    return (
-        <mesh ref={modelRef}>
-            <primitive object={scene} position-y={0} rotation-y={0} />
-        </mesh>
-    );
+  const { scene } = useGLTF(url);
+  const modelRef = useRef();
+  useFrame(() => {
+    if (modelRef.current) {
+      modelRef.current.rotation.y += 0.001;
+      modelRef.current.rotation.x += 0.001;
+    }
+  });
+  return (
+    <mesh ref={modelRef}>
+      <primitive object={scene} position={[0, 0, 0]} rotation={[0, 0, 0]} />
+    </mesh>
+  );
 };
 
 const ModelViewer = ({ url }) => {
-    return (
-        <Canvas
-            frameloop="demand"
-            shadows
-            dpr={[1, 2]}
-            camera={{
-                fov: 30,
-                near: 0.1,
-                far: 200,
-                position: [4, 2, 6],
-            }}
-            gl={{ preserveDrawingBuffer: true }}
-        >
-            <ambientLight intensity={0.5} />
-            <directionalLight position={[10, 10, 10]} intensity={1} />
-            <Suspense fallback={"jsdjdb"}>
-                <Model url={url} />
-            </Suspense>
-            <OrbitControls enableZoom={false}/>
-        </Canvas>
-    );
+  return (
+    <Canvas
+      frameloop="demand"
+      shadows
+      dpr={[1, 2]}
+      camera={{
+        fov: 30,
+        near: 0.1,
+        far: 200,
+        position: [4, 2, 6],
+      }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <ambientLight intensity={1} />
+      <directionalLight position={[10, 10, 10]} intensity={1.5} />
+      <Suspense fallback={"Loading..."}>
+        <Model url={url} />
+      </Suspense>
+      <OrbitControls enableZoom={true} />
+    </Canvas>
+  );
 };
 
 export default ModelViewer;
